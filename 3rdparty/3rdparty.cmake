bd46bfcecc97a167d7810f719e7d2fff0cf798ad
Eason <56984895+eason69@users.noreply.github.com>
2024-11-21 09:10:05 +0800

    增加优缺点

--- 3rdparty/3rdparty.cmake
+++
@@ -1,7 +0,0 @@
-# asio
-include_directories(${CMAKE_SOURCE_DIR}/3rdparty/asio)
-
-# openssl
-set(OPENSSL_ROOT_DIR "${CMAKE_SOURCE_DIR}/3rdparty/openssl")
-include_directories(${OPENSSL_ROOT_DIR}/include)
-link_directories(${OPENSSL_ROOT_DIR}/lib)
